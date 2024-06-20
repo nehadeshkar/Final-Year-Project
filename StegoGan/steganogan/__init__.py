f20fee6485831aaa9a451d067646cd88d9af58e1
nehadeshkar <nehadeshkar340@outlook.com>
2024-06-20 13:52:40 +0530

    Add files via upload

---
+++ StegoGan/steganogan/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for SteganoGAN."""
+
+__author__ = """PES MCOE"""
+__email__ = 'hbagul16@gmail.com'
+__version__ = '0.1.0-dev'
+
+from steganogan.models import SteganoGAN
+
+__all__ = ('SteganoGAN', )
